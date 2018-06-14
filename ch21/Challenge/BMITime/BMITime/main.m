2bc1e86e67d7b6ba8e37e35f4d1fb1cdf2d0ee79
Maksim Nosov <ozolc@yandex.ru>
2018-06-14 15:45:35 +0300

    Code from chapter 21

---
+++ ch21/Challenge/BMITime/BMITime/main.m
@@ -0,0 +1,74 @@
+//
+//  main.m
+//  BMITime
+//
+//  Created by Maksim Nosov on 13/06/2018.
+//  Copyright © 2018 Maksim Nosov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "BNREmployee.h"
+#import "BNRAsset.h"
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+        // Create an instance of BNREmployee objects
+        NSMutableArray *employees = [[NSMutableArray alloc] init];
+        
+        for (int i = 0; i < 10; i++) {
+            // Create an instance of BNREmployee
+            BNREmployee *mikey = [[BNREmployee alloc] init];
+            
+            // Give the instance variables interesting values
+            mikey.weightInKilos = 90 + i;
+            mikey.heightInMeters = 1.8 - i/10.0;
+            mikey.employeeID = i;
+            
+            // Put the employee in the employees array
+            [employees addObject:mikey];
+        }
+        
+        // Create 10 assets
+        for (int i = 0; i < 10; i++) {
+            // Create an asset
+            BNRAsset *asset = [[BNRAsset alloc] init];
+            
+            // Give it an interesting label
+            NSString *currentLabel = [NSString stringWithFormat:@"Laptop %d", i];
+            asset.label = currentLabel;
+            asset.resaleValue = 350 + i * 17;
+            
+            // Get a random number between 0 and 9 inclusive
+            NSUInteger randomIndex = random() % [employees count];
+            
+            // Find that employee
+            BNREmployee *randomEmployee = [employees objectAtIndex:randomIndex];
+            
+            // Assign the asset to the employee
+            [randomEmployee addAsset:asset];
+        }
+        
+
+        
+        // [employees removeObjectAtIndex:5];
+        
+        for (int i = 0; i < 10; i++)
+        {
+            // Get a random number
+            NSUInteger randomIndex = random() % [employees count];
+            
+            // Random employee chosen from employees array
+            BNREmployee *randomEmployee = [employees objectAtIndex:randomIndex];
+            
+            // Remove all assets from randomEmployee
+            NSLog(@"Employees: %@", randomEmployee);
+            NSLog(@"Giving up ownership of %@ employee\n", randomEmployee);
+            [randomEmployee removeAsset:randomEmployee];
+        }
+        
+        NSLog(@"Giving up ownership of arrays");
+        employees = nil;
+    }
+    return 0;
+}
