2cd7cbe8344e405d8c27fd434140ae50fd518f4c
Maksim Nosov <ozolc@yandex.ru>
2018-06-15 17:34:28 +0300

    Code from chapter 25

---
+++ ch25/Constants/Constants/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Constants
+//
+//  Created by Maksim Nosov on 15/06/2018.
+//  Copyright © 2018 Maksim Nosov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+        NSLog(@"\u03c0 is %f", M_PI);
+        NSLog(@"%d is larger", MAX(10, 12));
+        
+        NSLocale *here = [NSLocale currentLocale];
+        NSString *currency = [here objectForKey:NSLocaleCurrencyCode];
+        NSLog(@"Money is %@", currency);
+    }
+    return 0;
+}
