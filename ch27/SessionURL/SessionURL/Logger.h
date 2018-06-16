b496504440644a6a37ef260fa71b443770f9ff45
Maksim Nosov <ozolc@yandex.ru>
2018-06-16 11:29:01 +0300

    Code from chapter 27

---
+++ ch27/SessionURL/SessionURL/Logger.h
@@ -0,0 +1,13 @@
+//
+//  Logger.h
+//  SessionURL
+//
+//  Created by Maksim Nosov on 16/06/2018.
+//  Copyright © 2018 Maksim Nosov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Logger : NSObject <NSURLSessionDelegate, NSURLSessionDataDelegate>
+
+@end
