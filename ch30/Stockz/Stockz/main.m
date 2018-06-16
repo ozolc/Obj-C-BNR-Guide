d73737c812b751078f395ad63f78c8d6577669e5
Maksim Nosov <ozolc@yandex.ru>
2018-06-16 18:10:38 +0300

    Code from chapter 30

---
+++ ch30/Stockz/Stockz/main.m
@@ -0,0 +1,40 @@
+//
+//  main.m
+//  Stockz
+//
+//  Created by Maksim Nosov on 16/06/2018.
+//  Copyright © 2018 Maksim Nosov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+        // Create Property List to /tmp/stocks.plist in XML format
+        NSMutableArray *stocks = [[NSMutableArray alloc] init];
+        
+        NSMutableDictionary *stock;
+        
+        stock = [NSMutableDictionary dictionary];
+        [stock setObject:@"AAPL" forKey:@"symbol"];
+        [stock setObject:[NSNumber numberWithInt:200] forKey:@"shares"];
+        [stocks addObject:stock];
+        
+        stock = [NSMutableDictionary dictionary];
+        [stock setObject:@"GOOG" forKey:@"symbol"];
+        [stock setObject:[NSNumber numberWithInt:160] forKey:@"shares"];
+        [stocks addObject:stock];
+        
+        [stocks writeToFile:@"/tmp/stocks.plist" atomically:YES];
+        
+        // Read Property List from /tmp/stocks.plist
+        NSArray *stockList = [NSArray arrayWithContentsOfFile:@"/tmp/stocks.plist"];
+        
+        for (NSDictionary *d in stockList) {
+            NSLog(@"I have %@ shares of %@",
+                  [d objectForKey:@"shares"], [d objectForKey:@"symbol"]);
+        }
+    }
+    return 0;
+}
